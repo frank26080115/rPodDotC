ed2a7b8df29c74995b83aadd7e480d6565ac25fe
Frank Zhao <me@frank-zhao.com>
2016-10-07 00:03:52 -0700

    added 3d math for hover attitude

---
+++ math3d.c
@@ -0,0 +1,63 @@
+#include "math3d.h"
+#include <math.h>
+#include <stdlib.h>
+
+void hoverattitude_calculate(pod_coords_t* podcoords, laser_readings_t* readings, attitude_t* result)
+{
+	plane_t ground;
+	point3d_t vec1, vec2, cxprod;
+	rpod_float_t d;
+	int i;
+
+	// calculate two vectors in the plane
+	vec1.x = readings->results[0].x - readings->results[1].x;
+	vec1.y = readings->results[0].y - readings->results[1].y;
+	vec1.z = readings->results[0].z - readings->results[1].z;
+	vec2.x = readings->results[1].x - readings->results[2].x;
+	vec2.y = readings->results[1].y - readings->results[2].y;
+	vec2.z = readings->results[1].z - readings->results[2].z;
+
+	// calculate the cross product of the vectors
+	// to get a vector normal to the plane
+	cxprod.x = vec1.y * vec2.z - vec1.z * vec2.y;
+	cxprod.x = vec1.z * vec2.x - vec1.x * vec2.z;
+	cxprod.x = vec1.x * vec2.y - vec1.y * vec2.x;
+
+	// the normal vector should be pointed in the +Z direction
+	// tt affects which side of the plane has negative distances
+	if (cxprod.z < 0) {
+		cxprod.x *= -1;
+		cxprod.y *= -1; 
+		cxprod.z *= -1;
+	}
+
+	// plane in 3D: Ax + By + Cz + D = 0
+	// A, B, C is the vector normal to the plane
+	// use one of our original points to calculate D
+	d = -1 * (cxprod.x*readings->results[0].x + cxprod.y*readings->results[0].y + cxprod.z*readings->results[0].z);
+
+	ground.a = cxprod.x;
+	ground.b = cxprod.y;
+	ground.c = cxprod.z;
+	ground.d = d;
+
+	// calculate the height of each corner of the pod
+	// track the lowest point
+	result->min_height = 9999999;
+	for (i = 0; i < 4; i++)
+	{
+		rpod_float_t tmp;
+		point3d_t* corner = &podcoords->corner_pos[i];
+		// calculate distance from a point to a plane
+		tmp = result->corner_height[i] = (ground.a*corner->x + ground.b*corner->y + ground.c*corner->z + ground.d) / sqrt((double)(ground.a*ground.a + ground.b*ground.b + ground.c*ground.c));
+		if (tmp < result->min_height) {
+			result->min_height = tmp;
+		}
+	}
+
+	vect1.x = 1; vect1.y = 0; vect1.z = 0;
+	result->roll = acos((double)((vec1.x*plane.a + vec1.y*plane.b + vec1.z*plane.c) / sqrt((double)(plane.a*plane.a + plane.b*plane.b + plane.c*plane.c))))*(180.0/M_PI);
+
+	vect1.x = 0; vect1.y = 1; vect1.z = 0;
+	result->pitch = acos((double)((vec1.x*plane.a + vec1.y*plane.b + vec1.z*plane.c) / sqrt((double)(plane.a*plane.a + plane.b*plane.b + plane.c*plane.c))))*(180.0/M_PI);
+}
---
+++ math3d.h
@@ -0,0 +1,57 @@
+#ifndef _MATH3D_H_
+#define _MATH3D_H_
+
+#include "rpod_types.h"
+
+typedef struct
+{
+	rpod_float_t a;
+	rpod_float_t b;
+	rpod_float_t c;
+	rpod_float_t d;
+}
+plane_t;
+
+typedef struct
+{
+	rpod_float_t x;
+	rpod_float_t y;
+}
+point2d_t;
+
+typedef struct
+{
+	rpod_float_t x;
+	rpod_float_t y;
+	rpod_float_t z;
+}
+point3d_t;
+
+typedef struct
+{
+	rpod_float_t roll;
+	rpod_float_t pitch;
+	rpod_float_t center_height;
+	rpod_float_t min_height;
+	rpod_float_t corner_height[4];
+}
+attitude_t;
+
+// we need 3 laser readings to the ground to determine roll, pitch, and center height
+// we also need the positions of the corners just in case one engine is too close to the ground
+typedef struct
+{
+	point3d_t laser_pos[3];
+	point3d_t corner_pos[4];
+}
+pod_coords_t;
+
+typedef struct
+{
+	point3d_t results[3];
+}
+laser_readings_t;
+
+void hoverattitude_calculate(pod_coords_t* podcoords, laser_readings_t* readings, attitude_t* result);
+
+#endif
